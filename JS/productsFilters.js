cbf49bdd92ae07edf7e9f98e2d2c4fa80693c3e2
Pavel Bogatyi <murai32@gmail.com>
2022-08-15 10:44:59 +0300

    отладка

--- JS/productsFilters.js
+++ JS/productsFilters.js
@@ -165,7 +165,8 @@ class FilterByType extends Filter {
 			}
 
 			// Отправляем данные в функцию которая будет манипулировать с блоком карточек объектов
-			this.filterProjectsBlockByType(elem.getAttribute("data-filter-option")); 
+			console.log(typeof this.filterProjectsBlockByType(elem.getAttribute("data-filter-option"))); 
+			// this.filterProjectsBlockByType(elem.getAttribute("data-filter-option")); 
 
 			// Тут надо дать команду на установку класса-модификатара - active для элмента на который кликнули 
 			// и удаления этого класса модификатора с того элемента где он стоял
