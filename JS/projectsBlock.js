474a913036e3979ff9ac4a2623e7dacd0bdf8463
Pavel Bogatyi <murai32@gmail.com>
2022-08-29 14:37:52 +0300

    отладка

--- JS/projectsBlock.js
+++ JS/projectsBlock.js
@@ -83,8 +83,8 @@ function placeAwardRibon(){
 /* ИНИЦИИРУЕМ ВСЕ НЕОБХОДИМЫЕ СКРИПТЫ. Инициация должна находиться в самом низу*/
 document.addEventListener("DOMContentLoaded", function(event) {
     
-    projectsBlockChangeGrid();// Заужаем сетку блока Last-projects-block для вертикального заголовка блока    
-    projectsBlockImgWrap();// Оборачиваем изображения блока Last-projects-block
-    placeAwardRibon();// Помещаем ленточку awward на карточку Melon office
+    // projectsBlockChangeGrid();// Заужаем сетку блока Last-projects-block для вертикального заголовка блока    
+    // projectsBlockImgWrap();// Оборачиваем изображения блока Last-projects-block
+    // placeAwardRibon();// Помещаем ленточку awward на карточку Melon office
 
 })    
