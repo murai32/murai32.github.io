e68ed0eac7592be4000947fc1b880c190117300a
Pavel Bogatyi <murai32@gmail.com>
2022-12-13 02:38:37 +0300

    отладка

--- JS/productsFilters.js
+++ JS/productsFilters.js
@@ -135,6 +135,8 @@ class FilterByType extends Filter {
 
         if (typesList.findIndex(isWantedValue) != -1){
             typesList.splice(typesList.findIndex(isWantedValue),1); // Удаляем "Жилье" из массива
+
+            console.log(typesList);
             return typesList.splice(0, 0, "Жилье"); // Добавляем Жилье на нужную позицию
         } else {
             return list;
