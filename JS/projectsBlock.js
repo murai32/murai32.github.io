b395d542832b86d705062bc3cbc185d9e709d0fd
murai32 <murai32@gmail.com>
2026-05-31 18:53:36 +0300

    Исправление ошибок

--- JS/projectsBlock.js
+++ JS/projectsBlock.js
@@ -146,8 +146,8 @@ function placeAwardRibon() {
 
 /* ОБРАБАТЫВАЕМ И ДОБАВЛЯЕМ СНИППЕТ "ПРОДВИГАЕМЫЙ ПРОЕКТ" В БЛОК "ПОСЛЕДНИЕ ПРОЕКТЫ"*/
 function addPromotedProject(){    
-    const projectsBlock = document.querySelector(projectsBlockClass); // ссылка на блок "проекты"
-    const promotingPojectBlock = document.querySelector(projectsBlockClass); // ссылка на блок "проекты"
+    const projectsBlock = document.querySelector(_projectsBlockSelector); // ссылка на блок "последние проекты"
+    const promotingPojectBlock = document.querySelector(_promotingPojectBlockSelector); // ссылка на блок "проект которому дается промоушн"
 
 
     if (!document.querySelector(_promotingPojectBlockSelector).matches(_promotingPojectBlockSelector)) { 
